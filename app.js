a943e0ce20e11dd86445e0132d25d10d22089f4c
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-25 12:11:56 +0900

    social login

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const cors = require("cors");
+const session = require('express-session')
 
-const authRouter = require("./passport/kakao/kakao");
-const passportKakao = require("./passport");
-const gitHub = require("./auth/github")
+const authRouter = require("./Social/kakao-auth/kakao/kakao");
+const passportKakao = require("./Social/kakao-auth");
+const gitHub = require("./Social/github-auth/github")
+const connect = require("./schemas")
 
 
 passportKakao();
-gitHub()
+gitHub();
+connect();
 
-
+app.use(session({secret:'MySecret', resave: false, saveUninitialized:true}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 app.use("/oauth", authRouter);
 
+
+app.use('/', require('./Social/google-auth/routes/main'));
+app.use('/auth', require('./Social/google-auth/routes/auth'));
 
 app.listen(port, () => {
   console.log(port, "서버가 연결되었습니다.");
