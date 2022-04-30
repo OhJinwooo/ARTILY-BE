0e7a0c84e8b1815b2a0fdf57b0e16b54e997e890
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-04-30 17:06:15 +0900

    Update kakao.js

--- kakao-auth/kakao/kakao.js
+++ kakao-auth/kakao/kakao.js
@@ -25,33 +25,3 @@ const kakaoCallback = (req, res, next) => {
 };
 router.get("/kakao/callback", kakaoCallback);
 module.exports = router;
-
-// const express = require("express");
-// const router = express.Router();
-// const passport = require("passport");
-// const jwt = require("jsonwebtoken");
-
-// router.get("/kakao", passport.authenticate("kakao"));
-
-// const kakaoCallback = (req, res, next) => {
-//   passport.authenticate(
-//     "kakao",
-//     { failureRedirect: "/" },
-//     (err, user, info) => {
-//       if (err) return next(err);
-//       console.log("콜백~~~");
-//       const { userId, nickname } = user;
-//       const token = jwt.sign({ userId: userId }, "velog-secret-key");
-
-//       result = {
-//         token,
-//         userId: userId,
-//         nickname: nickname,
-//       };
-//       console.log(result);
-//       res.send({ user: result });
-//     }
-//   )(req, res, next);
-// };
-// router.get("/kakao/callback", kakaoCallback);
-// module.exports = router;
