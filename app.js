0ef63c1d9b815ea0084afbfbf0018d3b861aa91c
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-03 01:22:07 +0900

    naver social test

--- app.js
+++ app.js
@@ -29,6 +29,64 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use(passport.initialize());
 app.use(passport.session());
 
+const client_id = process.env.naverClient_id;
+const client_secret = process.env.naverSecret;
+const state = "RANDOM_STATE";
+const redirectURI = encodeURI("http://localhost:3000/oauth/naver/callback");
+const api_url = "";
+app.get("/naver", function (req, res) {
+  console.log("naver 접근함");
+  api_url =
+    "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
+    client_id +
+    "&redirect_uri=" +
+    redirectURI +
+    "&state=" +
+    state;
+  res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+  res.end(
+    "<a href='" +
+      api_url +
+      "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
+  );
+});
+app.get("/naver/callback", function (req, res) {
+  console.log("callback 접근");
+  code = req.query.code;
+  state = req.query.state;
+  api_url =
+    "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=" +
+    client_id +
+    "&client_secret=" +
+    client_secret +
+    "&redirect_uri=" +
+    redirectURI +
+    "&code=" +
+    code +
+    "&state=" +
+    state;
+  var request = require("request");
+  var options = {
+    url: api_url,
+    headers: {
+      "X-Naver-Client-Id": client_id,
+      "X-Naver-Client-Secret": client_secret,
+    },
+  };
+  request.get(options, function (error, response, body) {
+    console.log("get", body);
+    if (!error && response.statusCode == 200) {
+      console.log("if get", body);
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      console.log("else", response);
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+
 https: app.listen(port, () => {
   console.log(port, "서버가 연결되었습니다.");
 });
--- naver-auth/login.js
+++ naver-auth/login.js
@@ -70,64 +70,64 @@
 // });
 
 // 네이버 로그인 Node.js 예제는 1개의 파일로 로그인요청 및 콜백 처리를 모두합니다.
-const express = require("express");
-const router = express.Router();
-const client_id = process.env.naverClient_id;
-const client_secret = process.env.naverSecret;
-const state = "";
-const redirectURI = encodeURI("http://localhost:3000/oauth/naver/callback");
-const api_url = "";
-router.get("/naver", function (req, res) {
-  console.log("naver 접근함");
-  api_url =
-    "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
-    client_id +
-    "&redirect_uri=" +
-    redirectURI +
-    "&state=" +
-    state;
-  res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
-  res.end(
-    "<a href='" +
-      api_url +
-      "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
-  );
-});
-router.get("/naver/callback", function (req, res) {
-  console.log("callback 접근");
-  code = req.query.code;
-  state = req.query.state;
-  api_url =
-    "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=" +
-    client_id +
-    "&client_secret=" +
-    client_secret +
-    "&redirect_uri=" +
-    redirectURI +
-    "&code=" +
-    code +
-    "&state=" +
-    state;
-  var request = require("request");
-  var options = {
-    url: api_url,
-    headers: {
-      "X-Naver-Client-Id": client_id,
-      "X-Naver-Client-Secret": client_secret,
-    },
-  };
-  request.get(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
-      console.log(body);
-      res.end("body", body);
-    } else {
-      console.log("res", response);
-      res.status(response.statusCode).end();
-      console.log("error = " + response.statusCode);
-    }
-  });
-});
+// const express = require("express");
+// const router = express.Router();
+// const client_id = process.env.naverClient_id;
+// const client_secret = process.env.naverSecret;
+// const state = "";
+// const redirectURI = encodeURI("http://localhost:3000/oauth/naver/callback");
+// const api_url = "";
+// router.get("/naver", function (req, res) {
+//   console.log("naver 접근함");
+//   api_url =
+//     "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=" +
+//     client_id +
+//     "&redirect_uri=" +
+//     redirectURI +
+//     "&state=" +
+//     state;
+//   res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+//   res.end(
+//     "<a href='" +
+//       api_url +
+//       "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
+//   );
+// });
+// router.get("/naver/callback", function (req, res) {
+//   console.log("callback 접근");
+//   code = req.query.code;
+//   state = req.query.state;
+//   api_url =
+//     "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=" +
+//     client_id +
+//     "&client_secret=" +
+//     client_secret +
+//     "&redirect_uri=" +
+//     redirectURI +
+//     "&code=" +
+//     code +
+//     "&state=" +
+//     state;
+//   var request = require("request");
+//   var options = {
+//     url: api_url,
+//     headers: {
+//       "X-Naver-Client-Id": client_id,
+//       "X-Naver-Client-Secret": client_secret,
+//     },
+//   };
+//   request.get(options, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+//       console.log(body);
+//       res.end("body", body);
+//     } else {
+//       console.log("res", response);
+//       res.status(response.statusCode).end();
+//       console.log("error = " + response.statusCode);
+//     }
+//   });
+// });
 // router.listen(3000, function () {
 //   console.log("http://127.0.0.1:3000/naver app listening on port 3000!");
 // });
