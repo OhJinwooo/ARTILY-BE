b2e00cac14e282a27141f669df023c681813f56d
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-03 18:42:24 +0900

    db

--- schemas/index.schemas.js
+++ schemas/index.schemas.js
@@ -1,32 +1,14 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const connect = () => {
   mongoose
-    // .connect("mongodb://54.180.96.227:27017/artin", {
-    .connect(
-      "mongodb+srv://sdnzmzm5:01040104@cluster0.ehvln.mongodb.net/ARTILY?retryWrites=true&w=majority",
-      {
-        ignoreUndefined: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    .connect(process.env.MONGODBURL, {
+      ignoreUndefined: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .catch((err) => {
       console.error(err);
     });
 };
 module.exports = connect;
-
-// const mongoose = require("mongoose");
-// const connect = () => {
-//   mongoose
-//     .connect(
-//       "mongodb://localhost:27017/velog",
-//       // 이후 배포 시 변경 필요.
-//       { ignoreUndefined: true }
-//     )
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// };
-
-// module.exports = connect;
