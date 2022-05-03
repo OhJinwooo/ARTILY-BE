3946559f5e40401cee92f7a2d11d59819ea85262
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-03 20:41:33 +0900

    Update index.schemas.js

--- schemas/index.schemas.js
+++ schemas/index.schemas.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const connect = () => {
   mongoose
-    .connect(process.env.MONGODBURL, {
-      ignoreUndefined: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      "mongodb+srv://sdnzmzm5:01040104@cluster0.ehvln.mongodb.net/ARTILY?retryWrites=true&w=majority",
+      {
+        ignoreUndefined: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
     .catch((err) => {
       console.error(err);
     });
