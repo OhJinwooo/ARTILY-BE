6be490c63bb91db5ede4b2eeb029b7d311f05807
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-04 21:42:11 +0900

    mypage 추가(미완)

--- routes/controllers/mypage.controllers.js
+++ routes/controllers/mypage.controllers.js
@@ -1,7 +1,33 @@
 const User = require("../../schemas/user.schemas");
 
-const mypage = async (req, res) => {
-  const { userId } = res.locals.user;
+const postProfile = async (req, res) => {
+  const { user } = res.locals;
+  const { profileUrl, profile, nickname, snsUrl } = req.body;
+  const userId = user.userId;
+
+  try {
+    await User.updateOne(
+      {
+        userId,
+      },
+      {
+        $set: {
+          nickname,
+          profileUrl,
+          address,
+          profile,
+          snsUrl,
+        },
+      }
+    );
+    res.status(201).json({ success: true });
+  } catch (error) {
+    res.sattus(400).send("작성 실패");
+  }
+};
+
+const getProfile = async (req, res) => {
+  const userId = res.locals.user;
   try {
     const myprofile = await User.findOne({ userId });
     console.log(myprofile);
@@ -12,6 +38,35 @@ const mypage = async (req, res) => {
   }
 };
 
+const updateProfile = async (req, res) => {
+  const { user } = res.locals;
+  const { nickname, profileUrl, address, profile, inquiry, snsUrl } = req.body;
+  const userId = user.userId;
+
+  try {
+    await User.updateOne(
+      {
+        userId,
+      },
+      {
+        $set: {
+          nickname,
+          profileUrl,
+          address,
+          profile,
+          inquiry,
+          snsUrl,
+        },
+      }
+    );
+    res.status(201).json({ success: true });
+  } catch (error) {
+    res.sattus(400).send("수정 실패");
+  }
+};
+
 module.exports = {
-  mypage,
+  postProfile,
+  getProfile,
+  updateProfile,
 };
--- routes/mypage.router.js
+++ routes/mypage.router.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const middleware = require("../middleware/authMiddleWare");
-const { mypage } = require("./controllers/mypage.controllers");
+const {
+  postProfile,
+  getProfile,
+  updateProfile,
+} = require("./controllers/mypage.controllers");
 
-router.get("/mypage", middleware, mypage);
+//프로필 등록
+router.patch("/profile", middleware, postProfile);
+
+//마이페이지 조회
+router.get("/profile", middleware, getProfile);
+
+//마이페이지 프로필 수정
+router.patch("/profile/update", middleware, updateProfile);
 
 module.exports = router;
