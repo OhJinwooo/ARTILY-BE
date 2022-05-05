31fcc866b0655d9e090ab14faf6887cefd7b6b6c
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-06 02:36:02 +0900

    multer test

--- kakao-auth/kakao/kakao.js
+++ kakao-auth/kakao/kakao.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
+const upload = require("../../routes/multer/uploads");
 
 router.get(
   "/kakao",
