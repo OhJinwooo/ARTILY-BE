5f913d6f1d5807016d32eab8891c78153ebc779c
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-05-07 00:18:30 +0900

    .env삭제

---
+++ routes/config/s3.js
@@ -0,0 +1,10 @@
+const AWS = require("aws-sdk");
+require("dotenv").config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: process.env.region,
+});
+
+module.exports = s3;
