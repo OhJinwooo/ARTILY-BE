5c6b5327f09329164f34ab7dcb7ef654bc6191a5
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-05-07 11:18:26 +0900

    b

--- routes/post.router.js
+++ routes/post.router.js
@@ -1,27 +1,27 @@
- const express = require("express");
- const router = express.Router();
- const upload = require("../multer/uploads");
- const {
-   artPost,
-   artDetail,
-   artStore,
-   artUpdate,
-   getHome,
-   artdelete,
-   marckupCnt
- } = require("./controllers/post.controllers");
- router.get("/post", getHome);
+const express = require("express");
+const router = express.Router();
+const upload = require("./multer/uploads");
+const {
+  artPost,
+  artDetail,
+  artStore,
+  artUpdate,
+  getHome,
+  artdelete,
+  marckupCnt,
+} = require("./controllers/post.controllers");
+router.get("/post", getHome);
 
- router.get("/post/store", artStore);
+router.get("/post/store", artStore);
 
- router.get("/post/:postId", artDetail);
+router.get("/post/:postId", artDetail);
 
- router.post("/post", upload.array("img"), artPost);
+router.post("/post", upload.array("img"), artPost);
 
- router.put("/post/:postId", upload.array("img"), artUpdate);
+router.put("/post/:postId", upload.array("img"), artUpdate);
 
- router.delete("/post/:postId",artdelete);
+router.delete("/post/:postId", artdelete);
 
-router.post('/cnt/:postId',marckupCnt)
+router.post("/cnt/:postId", marckupCnt);
 
 module.exports = router;
