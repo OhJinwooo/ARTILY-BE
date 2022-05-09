2e91509eb0e1abd7ef6bd5587cfe3b00d807f3dd
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-09 22:45:54 +0900

    dotenv 위치 변경

--- naver-auth/naver/naver.js
+++ naver-auth/naver/naver.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const upload = require("../../routes/multer/uploads");
-require("dotenv").config();
 
 router.get(
   "/naver",
