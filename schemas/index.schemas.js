7f052d25c781a076320934fe0dc023a2ca27c438
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-09 20:42:10 +0900

    MONGODBURL env 추가

--- schemas/index.schemas.js
+++ schemas/index.schemas.js
@@ -20,14 +20,11 @@ require("dotenv").config();
 const connect = () => {
   mongoose
     // .connect("mongodb://54.180.96.227:27017/artin", {
-    .connect(
-      "mongodb+srv://sdnzmzm5:01040104@cluster0.ehvln.mongodb.net/ARTILY?retryWrites=true&w=majority",
-      {
-        ignoreUndefined: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    .connect(process.env.MONGODBURL, {
+      ignoreUndefined: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .catch((err) => {
       console.error(err);
     });
