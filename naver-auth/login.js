d639a891079bb2ddf3e41bfd8a7dbc92b91da8de
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-10 16:23:36 +0900

    try catch 제거 test

--- naver-auth/login.js
+++ naver-auth/login.js
@@ -17,46 +17,46 @@ module.exports = () => {
       async (accessToken, refreshToken, profile, done) => {
         console.log("NaverStrategy");
 
-        try {
-          console.log("try in", profile);
-          const exUser = await User.findOne({
+        // try {
+        console.log("try in", profile);
+        const exUser = await User.findOne({
+          userId: profile.id,
+          provider: "naver",
+        });
+        // clientID에 카카오 앱 아이디 추가
+        // callbackURL: 카카오 로그인 후 카카오가 결과를 전송해줄 URL
+        // accessToken, refreshToken : 로그인 성공 후 카카오가 보내준 토큰
+        // profile: 카카오가 보내준 유저 정보. profile의 정보를 바탕으로 회원가입
+
+        let profileImage = "";
+        let nickname = "";
+        let address = "";
+        let introduce = "";
+        let role = true;
+        if (exUser) {
+          console.log("로그인", exUser);
+          done(null, exUser);
+        } else {
+          const user = {
+            refreshToken: refreshToken,
+            accessToken: accessToken,
             userId: profile.id,
             provider: "naver",
-          });
-          // clientID에 카카오 앱 아이디 추가
-          // callbackURL: 카카오 로그인 후 카카오가 결과를 전송해줄 URL
-          // accessToken, refreshToken : 로그인 성공 후 카카오가 보내준 토큰
-          // profile: 카카오가 보내준 유저 정보. profile의 정보를 바탕으로 회원가입
+            type,
+            profileImage,
+            nickname,
+            address,
+            type: "new",
+            introduce,
+            role,
+          };
+          await User.create(user);
 
-          let profileImage = "";
-          let nickname = "";
-          let address = "";
-          let introduce = "";
-          let role = true;
-          if (exUser) {
-            console.log("로그인", exUser);
-            done(null, exUser);
-          } else {
-            const user = {
-              refreshToken: refreshToken,
-              accessToken: accessToken,
-              userId: profile.id,
-              provider: "naver",
-              type,
-              profileImage,
-              nickname,
-              address,
-              type: "new",
-              introduce,
-              role,
-            };
-            await User.create(user);
-
-            done(null, user);
-          }
-        } catch {
-          console.error(error);
+          done(null, user);
         }
+        // } catch {
+        //   console.error(error);
+        // }
       }
     )
   );
