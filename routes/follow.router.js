d9524395036ab0cf290e82fc7ded9b26a14d8e26
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-18 10:29:08 +0900

    다른 유저의 팔로우 리스트조회 추가

--- routes/follow.router.js
+++ routes/follow.router.js
@@ -3,17 +3,25 @@ const router = express.Router();
 const middleware = require("../middleware/authMiddleWare");
 const {
   addfollow,
-  getFollow,
-  getFollower,
+  follow,
+  follower,
+  myFollow,
+  myFollower,
 } = require("./controllers/follow.controllers");
 
 //팔로우
 router.post("/follow/:followId", middleware, addfollow);
 
 //내 팔로우 리스트 조회
-router.get("/follow/followlist", middleware, getFollow);
+router.get("/follow/myfollowlist", middleware, myFollow);
 
 //내 팔로워 리스트 조회
-router.get("/follow/followerlist", middleware, getFollower);
+router.get("/follow/myfollowerlist", middleware, myFollower);
+
+//다른 유저 팔로우 리스트 조회
+router.get("/follow/followlist/:userId", middleware, follow);
+
+//다른 유저 리스트 조회
+router.get("/follow/followerlist/:userId", middleware, follower);
 
 module.exports = router;
