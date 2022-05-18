9615605a97248370d51b5ffd5e1e3001c7d88072
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-18 21:19:49 +0900

    최신 채팅기록 불러오기

--- routes/controllers/chat.controllers.js
+++ routes/controllers/chat.controllers.js
@@ -45,22 +45,13 @@
 
 const Chat = require("../../schemas/chat.schemas");
 const ChatData = require("../../schemas/chatData.schemas");
+
 const chatData = async (req, res) => {
   const { userId } = res.locals.user;
+
   const roomUser = await Chat.find({});
-  console.log("roomUser", roomUser);
-  const lastMessage = "";
-  for (let i = 0; i < roomUser.length; i++) {
-    let message = roomUser[i].messages;
-    console.log("message", message);
-    if (message.length !== 0) {
-      const lastMessage = message[message.length - 1];
-      console.log("last", lastMessage);
-      console.log("lastMessage.message", lastMessage.message);
-    }
-    await Chat.updateOne({}, { $set: { lastMessage: lastMessage.message } });
-    await Chat.updateOne({}, { $set: { lastTime: lastMessage.time } });
-  }
+  // console.log("roomUser", roomUser);
+
   let chatRoomName = [];
   for (let i = 0; i < roomUser.length; i++) {
     const a = roomUser[i].roomName;
@@ -72,9 +63,46 @@ const chatData = async (req, res) => {
     // console.log("chattingUser", chattingUser);
     // console.log("chatRoomName", chatRoomName);
   }
-  res.json({ chatRoomName });
+
+  let lastMessage = "";
+  for (let i = 0; i < chatRoomName.length; i++) {
+    const message = chatRoomName[i].messages;
+    const roomName = chatRoomName[i].roomName;
+    console.log("message", roomName);
+    if (message.length !== 0) {
+      lastMessage = message[message.length - 1];
+      console.log("last", lastMessage);
+      console.log("lastMessage.message", lastMessage.message);
+    }
+    const a = await Chat.updateOne(
+      { roomName },
+      { $set: { lastMessage: lastMessage.message } }
+    );
+    console.log("@@@@@@@@@@@", a);
+    await Chat.updateOne(
+      { roomName },
+      { $set: { lastTime: lastMessage.time } }
+    );
+  }
+
+  const newRoomUser = await Chat.find(
+    {},
+    "roomName post targetUser newMessage lastMessage lastTime"
+  );
+  const newChat = [];
+  for (let i = 0; i < newRoomUser.length; i++) {
+    const a = newRoomUser[i].roomName;
+    const b = newRoomUser[i];
+    let chattingUser = a.includes(userId);
+    if (chattingUser === true) {
+      newChat.push(b);
+    }
+    // console.log("chattingUser", chattingUser);
+    // console.log("chatRoomName", chatRoomName);
+  }
+
+  res.json({ newChat });
 };
 module.exports = {
   chatData,
 };
-// includes(userId);
