25970225f564799cf5a8d357a975fdd99e5539bb
Jinwoo Oh <101077454+OhJinwooo@users.noreply.github.com>
2022-05-19 19:55:46 +0900

    socket.js disconnected 수정

--- socket.js
+++ socket.js
@@ -210,11 +210,16 @@ module.exports = (server) => {
     });
     socket.on("disconnect", async () => {
       const user = await chatData.findOne({ userId: socket.id });
-      await chatData.updateOne(
+      const disUser = await chatData.updateOne(
         { userId: user.userId },
         { $set: { connected: false } }
       );
-      socket.broadcast.emit("user disconnected", user.userId, user.connected);
+      const newUser = await chatData.findOne({ userId: socket.id });
+      socket.broadcast.emit(
+        "user disconnected",
+        newUser.userId,
+        newUser.connected
+      );
     });
   });
 };
