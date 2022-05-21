071cb30263b7ea43c15dbbcd02ad42895025dc03
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-05-21 16:38:40 +0900

    in

--- routes/controllers/post.controllers.js
+++ routes/controllers/post.controllers.js
@@ -118,6 +118,7 @@ const artStore = async (req, res) => {
         }
       }
       if (Array.isArray(artPost) && artPost.length === 0) {
+        console.log("데이터 없다.")
         return res.status(200).json({
           respons: "fail",
           msg: "데이터 없음",
