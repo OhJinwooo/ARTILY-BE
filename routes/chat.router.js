f9e3a9529cbf407e7ee144e64d31f6389ac8cc59
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-05-21 18:19:03 +0900

    주석해제

--- routes/chat.router.js
+++ routes/chat.router.js
@@ -1,4 +1,4 @@
-/* const express = require("express");
+const express = require("express");
 const router = express.Router();
 const middleswares = require("../middleware/authMiddleWare");
 const { chatData, messages } = require("./controllers/chat.controllers");
@@ -8,4 +8,3 @@ router.get("/chat/data", middleswares, chatData);
 router.get("/chat/messages/:roomName", middleswares, messages);
 
 module.exports = router;
- */
