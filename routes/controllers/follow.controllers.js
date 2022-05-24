0fdded9181c2b7fadc425e2f2f9f371a77f1b043
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-05-24 20:53:38 +0900

    sss

--- routes/controllers/follow.controllers.js
+++ routes/controllers/follow.controllers.js
@@ -25,14 +25,14 @@ const addfollow = async (req, res) => {
         followName,
         profileImage,
       });
-      
+      console.log('follow',follow)
       await myFollow.updateOne({ $inc: { followCnt: 1 } });
       await followUser.updateOne({ $inc: { followerCnt: 1 } });
 
       res.send({ success: true, msg: "팔로잉" });
     } else {
       const follow = await Follow.deleteOne({ userId, followId });
-
+      console.log('follow exit',follow)
       await myFollow.updateOne({ $inc: { followCnt: -1 } });
       await followUser.updateOne({ $inc: { followerCnt: -1 } });
       res.send({ success: true, msg: "팔로우 취소" });
