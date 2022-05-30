ca4a12994b3066888bd40d09e54aeb3b9dabea4a
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2022-05-30 11:46:41 +0900

    review무한스크롤test

--- routes/controllers/review.controllers.js
+++ routes/controllers/review.controllers.js
@@ -15,28 +15,28 @@ const uuid = () => {
 // 리뷰 조회(무한 스크롤)
 const review = async (req, res) => {
   try {
-    // const data = req.query;
-    // console.log("page", data.page);
-    // console.log("limit", data.limit);
-    //infinite scroll 핸들링
+    const data = req.query;
+    console.log("page", data.page);
+    console.log("limit", data.limit);
+    // infinite scroll 핸들링
     // 변수 선언 값이 정수로 표현
-    // let page = Math.max(1, parseInt(data.page));
-    // let limit = Math.max(1, parseInt(data.limit));
-    //NaN일때 값지정 ??
-    // page = !isNaN(page) ? page : 1;
-    // limit = !isNaN(limit) ? limit : 6;
-    //제외할 데이터 지정 == 다음 페이지 시작점
-    //let skip = (page - 1) * limit;
+    let page = Math.max(1, parseInt(data.page));
+    let limit = Math.max(1, parseInt(data.limit));
+    // NaN일때 값지정
+    page = !isNaN(page) ? page : 1;
+    limit = !isNaN(limit) ? limit : 6;
+    // 제외할 데이터 지정 == 다음 페이지 시작점
+    let skip = (page - 1) * limit;
 
-    // const reviews = await Review.find(
-    //   {},
-    //   "createdAt reviewId nickname profileImage reviewTitle reviewContent images likeCnt seller.category"
-    // )
-    //   .sort("-createdAt")
-    //   .skip(skip)
-    //   .limit(limit);/
+    const reviews = await Review.find(
+      {},
+      "createdAt reviewId nickname profileImage reviewTitle reviewContent images likeCnt seller.category"
+    )
+      .sort("-createdAt")
+      .skip(skip)
+      .limit(limit);
 
-    const reviews = await Review.find({}).sort("-createdAt");
+    // const reviews = await Review.find({}).sort("-createdAt");
     if (reviews.length) {
       for (let review of reviews) {
         const imgs = await ReviewImages.findOne({
