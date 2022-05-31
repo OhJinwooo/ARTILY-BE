c13c9cc8d83296688bcf8d18a471def4235da2f4
chasungbin <97427142+chasungbin@users.noreply.github.com>
2022-05-31 17:01:15 +0900

    joi

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use("/api", [
   blackListRouter,
   postRouter,
   followRouter,
-/*   chatRouter, */
+  chatRouter,
 ]);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.get("/", (req, res) => {
